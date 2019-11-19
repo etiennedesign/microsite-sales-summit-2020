7ae5c67cfb6719bdcff56ef962a340773b4f7051
Etienne <etienneleprette@gmail.com>
2019-11-19 17:23:43 -0500

    content

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ jQuery(function ($) {
 
    if ($('.countdown').length > 0) {
       $(".countdown").jCounter({
-         date: '21 October 2019 12:00:00',
+         date: '7 June 2020 09:00:00',
          fallback: function () {
             console.log("count finished!")
          }
